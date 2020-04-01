d3d577d53ac6586fcbc86f8b818c7be536a4d81d
Mathieu Basille <basille@ufl.edu>
2020-04-01 20:44:53 +0200

    Proposal.

---
+++ ghgenerate.R
@@ -0,0 +1,9 @@
+proposal<-"sftrack, part II: Developer task force for a broad adoption"
+sponsor <- "R Consortium's ISC"
+proposal.file<-"isc-proposal.Rmd"
+author<-"Mathieu Basille"
+
+rmarkdown::render(proposal.file, output_format="pdf_document",
+                  output_dir="out", quiet=TRUE)
+rmarkdown::render(proposal.file, output_format="html_document",
+                  output_dir="out", quiet=TRUE)
